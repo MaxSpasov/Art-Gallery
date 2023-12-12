476b1fe5d4691969d8e7fed9db5bf6b10fd13b28
MaxSpasov <114147361+MaxSpasov@users.noreply.github.com>
2023-12-12 17:06:47 +0200

    Changes

--- src/controllers/homeController.js
+++ src/controllers/homeController.js
@@ -1,4 +1,7 @@
 const router = require('express').Router();
+const photoManager = require('../managers/photoManager');
+const { isAuth } = require('../middlewares/authMiddleware')
+
 
 router.get('/', (req, res) => {
     res.render('home')
@@ -6,6 +9,12 @@ router.get('/', (req, res) => {
 
 router.get('/404', (req, res) => {
     res.render('404');
+});
+
+router.get('/profile', isAuth, async (req, res) => {
+
+
+    res.render('profile');
 });
 
 module.exports = router;
